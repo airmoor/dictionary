699ccfce6b0e7d8e7a526878e7a8b184a362119a
Anastasia <nastia.schatskaya@gmail.com>
2020-06-14 22:48:34 +0300

    add audio

--- src/components/Word.tsx
+++ src/components/Word.tsx
@@ -1,6 +1,7 @@
 import axois from 'axios';
 import React, { useState, useEffect } from 'react';
-import {Typography, makeStyles, Box} from '@material-ui/core';
+import {Typography, makeStyles, Box, IconButton} from '@material-ui/core';
+import VolumeUpIcon from '@material-ui/icons/VolumeUp'
 import DefinitionList from './DefinitionList';
 
 const useStyles = makeStyles((theme) => ({
@@ -34,9 +35,8 @@ export default function Word ({word}:AppProps)  {
     const [values, setValues] = useState({
         types: Array<any> ([]),
         transcription: '',
-        audio: ''
+        audioUrl: ''
     })
-  
     const KEY = 'ce4a9515-e40e-4234-8605-58c5475fd9dc'
     const URL = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json'
     
@@ -45,27 +45,9 @@ export default function Word ({word}:AppProps)  {
         .then( (response) => {
             console.log(response)
             let data = response.data
-            data = data.filter((item: { shortdef: string; }) => item.shortdef !='')
-            if (data.length===0) setNotFound(true)
-            if (typeof data[0]==='string') setNotFound(true)
-            if (typeof data[0]==='object') setNotFound(false)
-
-            let wordTypes: Array<any> = []
-
-            for (let elem of data) {
-                if (!wordTypes.includes(elem.fl)) wordTypes.push(elem.fl)
-            }
-
-            console.log(data)
-            console.log(wordTypes)
-
             setIsLoaded (true)
-            setWordData(data)
-            setValues({...values,   
-                        types: wordTypes,
-                        transcription:data[0].hwi.prs[0].mw, 
-                        audio:data[0].hwi.prs[0].sound.audio})
-            })
+            setData(data)
+        })
 
         .catch((error) => {
             console.log('error', error)
@@ -73,7 +55,46 @@ export default function Word ({word}:AppProps)  {
         })
     }, [word])
 
+
+    const setData = (data:Array<any>) => {
+        data = data.filter((item: { shortdef: string; }) => item.shortdef !='')
+        if (data.length===0)           setNotFound(true);
+        if (typeof data[0]==='string') setNotFound(true);
+        if (typeof data[0]==='object') setNotFound(false);
+        setWordData(data);
+        
+        let wordTypes: Array<any> = []
+            for (let elem of data) {
+                if (!wordTypes.includes(elem.fl)) wordTypes.push(elem.fl)
+            }
+
+        let audio=data[0].hwi.prs[0].sound.audio;
+        let audioUrl=makeSoundUrl(audio);
+        setValues({...values,   
+                    types: wordTypes,
+                    transcription:data[0].hwi.prs[0].mw, 
+                    audioUrl:audioUrl,
+        })
+    }
     
+    const makeSoundUrl = (audio:string) => {
+        const baseUrl='https://media.merriam-webster.com/audio/prons/en/us';
+        const format='mp3'; //mp3        wav        ogg
+        let subdirectory;
+        if (audio.slice(0,3)==='bix') subdirectory='bix';   //if audio begins with "bix", the subdirectory should be "bix",
+        else if (audio.slice(0,2)==='gg') subdirectory='gg'; //if audio begins with "gg", the subdirectory should be "gg",
+        else if (typeof audio.slice(0,1)==='number') subdirectory='number'; //if audio begins with a number or punctuation (eg, "_"), the subdirectory should be "number",
+        else if (audio.slice(0,1)==='_') subdirectory='number'; //if audio begins with a number or punctuation (eg, "_"), the subdirectory should be "number",
+        else subdirectory=audio.slice(0,1); // otherwise, the subdirectory is equal to the first letter of audio.
+        let url=`${baseUrl}/${format}/${subdirectory}/${audio}.${format}`;
+        return url;
+    }
+
+    const playSound = () => {
+        const audio = new Audio(values.audioUrl);
+        audio.play()
+    }
+
     return ( 
         <Box className={classes.word}>
             {
@@ -92,9 +113,15 @@ export default function Word ({word}:AppProps)  {
                     <Typography variant='h4'> {word} </Typography>
                     {
                         values.transcription && (
-                            <Typography variant='h6'> 
-                                /{values.transcription}/ 
-                            </Typography>
+                            <Box display='flex' justifyContent='center' alignItems='center'>
+                                <IconButton onClick={playSound}>
+                                    <VolumeUpIcon/>
+                                </IconButton>
+                                <Typography variant='h6'> 
+                                    /{values.transcription}/ 
+                                </Typography>
+                            </Box>
+                            
                         )
                     }
                    
